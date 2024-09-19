72d0a39b30befdf069969891e80832e366ede5bb
Ferriol Pey Comas <ferriol73pey@gmail.com>
2024-09-19 11:20:01 +0200

    feat: First Working version

--- main.ts
+++ main.ts
@@ -1,134 +1,84 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Plugin } from "obsidian";
+import { Timeline, DataSet } from "vis-timeline/standalone";
 
-// Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
+type TimelineEvent = {
+    group: string;
+    start: string;
+    end: string | null;
+    name: string;
+};
+
+
+export default class AwesomeTimelinePlugin extends Plugin {
+    async onload() {
+        // Register a new Markdown post processor for awesometimeline
+        this.registerMarkdownCodeBlockProcessor("awesometimeline", (source, el, ctx) => {
+            // Parse the timeline from the source
+            const timelineData = parseTimelineData(source);
+            renderTimeline(timelineData, el);
+        });
+    }
+
+    onunload() {
+        console.log("Unloading AwesomeTimeline plugin");
+    }
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+// Function to parse the timeline data
+function parseTimelineData(source: string): TimelineEvent[] {
+    const lines = source.trim().split("\n");
+
+    const events = [];
+    let groupName = "";
+
+    for (let line of lines) {
+        if (line.startsWith("#")) {
+            groupName = line.slice(1).trim();
+        }
+        else {
+            const match = line.match(/(\d{4}-\d{2}-\d{2}T?\d{2}:\d{2}:\d{2})?~?(\d{4}-\d{2}-\d{2}T?\d{2}:\d{2}:\d{2})?\s(.+)/);
+            if (match) {
+                events.push({
+                    group: groupName,
+                    start: match[1],
+                    end: match[2] || null,
+                    name: match[3]
+                });
+            }
+        }
+    }
+    return events;
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+// Function to render timeline
+function renderTimeline(events: TimelineEvent[], container: HTMLElement) {
 
-	async onload() {
-		await this.loadSettings();
+    // Use a timeline library to render the events, such as vis.js timeline
+    const timelineContainer = document.createElement("div");
+    timelineContainer.style.height = "400px";
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+    container.appendChild(timelineContainer);
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+    // Prepare timeline data
+    const items = new DataSet(
+        events.map((event, index) => ({
+            id: index,
+            content: event.name,
+            start: event.start,
+            end: event.end || undefined,
+            group: event.group,
+        }))
+    );
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+    const options = {
+        zoomable: true, // Enable zooming
+        zoomMin: 1000 * 60 * 60 * 24, // Minimum zoom level 1 day
+        zoomMax: 1000 * 60 * 60 * 24 * 365 * 100, // Max zoom 100 years
+        horizontalScroll: true,
+        verticalScroll: true,
+    };
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+    const timeline = new Timeline(timelineContainer, items, options);
+    // // 
 }
